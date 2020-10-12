8bc7cadf3b04241312f125f091505b72a48f0476
saudkhan <saud.alphasquad@gmail.com>
2020-10-12 17:39:33 +0500

    ideas completd

--- projects/Empires/test/src/components/Ideas.vue
+++ projects/Empires/test/src/components/Ideas.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class=" container shades ">
+  <div class="container shades">
     <h1>Ideas</h1>
-    <div>
+    <form @submit="submit">
       <v-text-field
-        label="Main input"
-        :rules="rules"
+        label="Title"
         hide-details="auto"
+        :value="title"
+        v-model="title"
       ></v-text-field>
-      <v-text-field label="Another input"></v-text-field>
+      <v-textarea
+        class="mt-5"
+        clearable
+        clear-icon="mdi-close-circle"
+        label="Description"
+        value="This is clearable text."
+        v-model="description"
+      ></v-textarea>
+      <v-btn elevation="5" x-large color="primary" type="submit"> Submit</v-btn>
+    </form>
+    <div class="d-flex flex-wrap">
+      <v-card
+        v-for="idea in ideas"
+        :key="idea"
+        elevation="2"
+        outlined
+        class="warning mt-10 pt-5 pl-5 mr-5"
+        style="height: 150px; width: 500px"
+      >
+        <h4>{{ idea.title }}</h4>
+        <div>{{ idea.description }}</div>
+      </v-card>
     </div>
   </div>
 </template>
@@ -15,6 +37,20 @@
 <script>
 export default {
   name: "ideas",
+  component: {},
+
+  data: () => ({
+    title: "",
+    description: "",
+    ideas: [],
+  }),
+  methods: {
+    submit() {
+      //   evt.preventDefault();
+      this.ideas.push({ title: this.title, description: this.description });
+      console.log("this ideas ", this.ideas);
+    },
+  },
 };
 </script>
 
